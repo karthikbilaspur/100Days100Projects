344b2937641807d6efbf8d9cc527b6b23b849278
V. Karthik <karthikv81291@gmail.com>
2026-03-25 18:15:23 +0530

    Delete ProfilePage directory

--- ProfilePage/script.js
+++
@@ -1,5 +0,0 @@
-// Add event listener to hero button
-document.querySelector('#hero button').addEventListener('click', () => {
-    // Scroll to about section
-    document.querySelector('#about').scrollIntoView({ behavior: 'smooth' });
-});
